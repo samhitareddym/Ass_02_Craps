acc7ff1d2127625ce8c1a203eb88cbb03ce6f15b
samhitareddy <samhitareddy0402@gmail.com>
2024-10-21 11:33:47 -0400

    ass 02 commit

--- src/Main.java
+++ src/Main.java
@@ -14,28 +14,28 @@ public class Main {
             System.out.println("Rolling the dice...");
 
             // Roll two dice and calculate sum
-            int dice1 = rollDice(rand);
-            int dice2 = rollDice(rand);
+            int dice1 = rand.nextInt(6) + 1;
+            int dice2 = rand.nextInt(6) + 1;
             int sum = dice1 + dice2;
 
-            // Show dice values and sum
-            System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
+            // Show rolled dice values and sum
+            System.out.println("You rolled " + dice1 + " + " + dice2 + " : " + sum);
 
             // Initial roll conditions
             if (sum == 7 || sum == 11) {
-                System.out.println("Natural! You win!");
+                System.out.println("Natural, You win!");
             } else if (sum == 2 || sum == 3 || sum == 12) {
                 System.out.println("Craps! You crapped out and lost.");
             } else {
                 int point = sum;
-                System.out.println("The point is now " + point + ".");
+                System.out.println("The point now is " + point + ".");
                 boolean pointAchieved = false;
 
                 // Subsequent rolls to match the point or roll a 7
                 while (!pointAchieved) {
                     System.out.println("Rolling for the point...");
-                    dice1 = rollDice(rand);
-                    dice2 = rollDice(rand);
+                    dice1 = rand.nextInt(6) + 1;
+                    dice2 = rand.nextInt(6) + 1;
                     sum = dice1 + dice2;
 
                     System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + sum);
@@ -53,17 +53,13 @@ public class Main {
             }
 
             // Ask if the user wants to play again
-            System.out.print("Would you like to play again? (yes/no): ");
+            System.out.print("Would you like to play again? (Y/N): ");
             String userResponse = scanner.next();
-            playAgain = userResponse.equalsIgnoreCase("yes");
+            playAgain = userResponse.equalsIgnoreCase("Y");
         }
 
         System.out.println("Thanks for playing!");
         scanner.close();
     }
-
-    // Method to simulate rolling a single die
-    private static int rollDice(Random random) {
-        return random.nextInt(6) + 1; // Returns a number between 1 and 6
-    }
 }
+
